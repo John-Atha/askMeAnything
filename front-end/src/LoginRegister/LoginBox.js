0beb6ff384470a0b18d1f3338e3bd0d223d30bb2
John-Atha <giannisj3@gmail.com>
2021-04-19 16:25:19 +0300

    Update login page

--- front-end/src/LoginRegister/LoginBox.js
+++ front-end/src/LoginRegister/LoginBox.js
@@ -7,23 +7,54 @@ import Button from 'react-bootstrap/Button';
 function LoginBox() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
 
     const handleUsername = (val) => {
         if (val.length<16) {
             setUsername(val);
+            setError(null);
+            setSuccess(null);
         }
+        else {
+            setError("Username should be less than 15 characters");
+            setSuccess(null);
+        }
+    }
+
+    const handleSubmit = (event) => {
+        if (username.length && password.length) {
+            console.log('submitted');
+            if (username=="atha" && password=="12345") {
+                setSuccess("Logged in successfully");
+                setError(null);
+            }
+            else {
+                setSuccess(null);
+                setError("Sorry, wrong credentials")
+            }
+        }
+        else {
+            setError("Fill both fields");
+            setSuccess(null);
+        }
+        event.preventDefault();
     }
 
     return(
         <div className="login-box margin-top-small center-content">
             <h5>Login</h5>
-            <div className="margin-top-small">
-                <input placeholder="Enter your username..." value={username} typ="text" onChange={(event) => handleUsername(event.target.value)} />
+            <div className="error-message">
+                {error}
             </div>
-            <div className="margin-top-smaller">
-                <input placeholder="Enter your password..." value={password} type="password" onChange={(event) => setPassword(event.target.value)} />
+            <div className="success-message">
+                {success}
             </div>
-            <Button className="margin-top-smaller" variant="outline-dark">Log in</Button>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Enter your username..." className="margin-top-smaller" value={username} typ="text" onChange={(event) => handleUsername(event.target.value)} />
+                <input placeholder="Enter your password..." className="margin-top-smaller" value={password} type="password" onChange={(event) => {setPassword(event.target.value); setError(null); setSuccess(null);}} />
+                <Button type="submit" className="margin-top-smaller" style={{'width': '40%', 'minWidth': 'min-content'}} variant="outline-dark" onClick={handleSubmit}>Log in</Button>
+            </form>
         </div>
     )
 }
