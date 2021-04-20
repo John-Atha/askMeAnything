94db865f720f41de85f532e7c63b85d44f4a1786
John-Atha <giannisj3@gmail.com>
2021-04-20 13:27:59 +0300

    Update navbar and default zoom level

--- front-end/src/Navbar/MyNavbar.js
+++ front-end/src/Navbar/MyNavbar.js
@@ -15,18 +15,12 @@ function MyNavbar() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/my">My AskMeAnything</Nav.Link>
+                    <Nav.Link href="#">My Profile</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#deets">Logout</Nav.Link>
+                    <Nav.Link href="/login">Logout</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
