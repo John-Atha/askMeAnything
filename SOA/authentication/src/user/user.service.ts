e820eb41b9190808644349c823522c0e9c945399
John-Atha <giannisj3@gmail.com>
2021-04-21 20:14:49 +0300

    Update getOne endpoint and add updateOne and deleteOne

--- SOA/authentication/src/user/user.service.ts
+++ SOA/authentication/src/user/user.service.ts
@@ -19,12 +19,14 @@ export class UserService {
 
   async findAll(params): Promise<User[]> {
     let res = await this.manager.find(User);
-    const start = parseInt(params.start) || 0;
-    const end = parseInt(params.end)+1 || res.length;
-    console.log(start);
-    console.log(end);
-    if (start > end) {
-      throw new BadRequestException('Start parameter should be less than the end');
+    const start = parseInt(params.start) - 1 || 0;
+    const end = parseInt(params.end) ||  ( parseInt(params.end)===0 ? 0 : res.length );
+    console.log(`params:`);
+    console.log(params);
+    console.log(`start: ${start}`);
+    console.log(`end: ${end}`);
+    if (start > end || start <= -1 || end === 0) {
+      throw new BadRequestException('Invalid parameters');
     }
     else {
       res = res.slice(start, end);
@@ -41,11 +43,24 @@ export class UserService {
     return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    return this.manager.transaction(async (manager) => {
+      const user = await manager.findOne(User, id);
+      if (!user) {
+        throw new NotFoundException(`User ${id} not found.`);
+      }
+      manager.merge(User, user, updateUserDto);
+      return manager.save(user);
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<void> {
+    return this.manager.transaction(async (manager) => {
+      const user = await manager.findOne(User, id);
+      if (!user) {
+        throw new NotFoundException(`User ${id} not found.`);
+      }
+      await manager.delete(User, id);
+    });
   }
 }
