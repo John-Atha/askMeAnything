9ebc2b0976f21414a6558472b470bd93775daed6
John-Atha <giannisj3@gmail.com>
2021-05-01 15:32:02 +0300

    Add post and delete question-upvote endpoints to quest-run

--- SOA/question-run/src/question-upvote/question-upvote.controller.ts
+++ SOA/question-run/src/question-upvote/question-upvote.controller.ts
@@ -5,22 +5,23 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
+  Request,
+  Delete, ParseIntPipe,
 } from '@nestjs/common';
 import { QuestionUpvoteService } from './question-upvote.service';
 import { CreateQuestionUpvoteDto } from './dto/create-question-upvote.dto';
 
-@Controller('question-upvote')
+@Controller('question-upvotes')
 export class QuestionUpvoteController {
   constructor(private readonly questionUpvoteService: QuestionUpvoteService) {}
 
   @Post()
-  create(@Body() createQuestionUpvoteDto: CreateQuestionUpvoteDto) {
-    return this.questionUpvoteService.create(createQuestionUpvoteDto);
+  create(@Request() req, @Body() createQuestionUpvoteDto: CreateQuestionUpvoteDto) {
+    return this.questionUpvoteService.create(req, createQuestionUpvoteDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.questionUpvoteService.remove(+id);
+  remove(@Request() req, @Param('id', ParseIntPipe) id: number) {
+    return this.questionUpvoteService.remove(req, id);
   }
 }
--- SOA/question-run/src/question-upvote/question-upvote.service.ts
+++ SOA/question-run/src/question-upvote/question-upvote.service.ts
@@ -1,13 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
 import { CreateQuestionUpvoteDto } from './dto/create-question-upvote.dto';
+import {InjectEntityManager} from "@nestjs/typeorm";
+import {EntityManager} from "typeorm";
+import {verify} from "../../general-methods/methods";
+import {User} from "../user/entities/user.entity";
+import {Question} from "../question/entities/question.entity";
+import {QuestionUpvote} from "./entities/question-upvote.entity";
 
 @Injectable()
 export class QuestionUpvoteService {
-  create(createQuestionUpvoteDto: CreateQuestionUpvoteDto) {
-    return 'This action adds a new questionUpvote';
+  constructor(@InjectEntityManager() private manager: EntityManager) {}
+
+  async create(req, createQuestionUpvoteDto: CreateQuestionUpvoteDto): Promise<QuestionUpvote> {
+    return this.manager.transaction(async (manager) => {
+      const user_id = verify(req);
+      const user = await manager.findOne(User, user_id);
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+      const question_id = createQuestionUpvoteDto.question.id;
+      const question = await manager.findOne(Question, question_id, { relations: ['owner'] });
+      if (!question) {
+        throw new NotFoundException(`Question '${question_id}' not found.`);
+      }
+      const upvote = await manager.create(QuestionUpvote, createQuestionUpvoteDto);
+      upvote.owner = user;
+      upvote.question = question;
+      return manager.save(upvote);
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} questionUpvote`;
+  async remove(req, id: number) {
+    return this.manager.transaction(async (manager) => {
+      const user_id = verify(req);
+      const user = await manager.findOne(User, user_id);
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+      const upvote = await manager.findOne(QuestionUpvote, id, { relations: ['owner'] });
+      if (!upvote) {
+        throw new NotFoundException(`Upvote '${id}' not found.`);
+      }
+      if (user.id !== upvote.owner.id) {
+        throw new BadRequestException(`Only the upvote's owner can delete it.`);
+      }
+      console.log(`I am deleting upvote with id=${id}.`);
+      return manager.delete(QuestionUpvote, id);
+    });
   }
 }
