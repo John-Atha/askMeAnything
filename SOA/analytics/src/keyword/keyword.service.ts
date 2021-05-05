2e5557b37277c8be0537ee08ba39b8d644c92faf
John-Atha <giannisj3@gmail.com>
2021-05-05 19:23:25 +0300

    Fix bug in keywords monthly questions endpoint

--- SOA/analytics/src/keyword/keyword.service.ts
+++ SOA/analytics/src/keyword/keyword.service.ts
@@ -3,7 +3,10 @@ import { InjectEntityManager } from '@nestjs/typeorm';
 import { Question } from '../question/entities/question.entity';
 import { EntityManager } from 'typeorm';
 import { Keyword } from './entities/keyword.entity';
-import { paginate, withCountQuestionsUpvotes } from '../../general-methods/methods';
+import {
+  paginate,
+  withCountQuestionsUpvotes,
+} from '../../general-methods/methods';
 
 @Injectable()
 export class KeywordService {
@@ -11,11 +14,20 @@ export class KeywordService {
 
   async findQuestionsMonthly(params, id, year, month): Promise<Question[]> {
     return this.manager.transaction(async (manager) => {
-      const keyword = await manager.findOne(Keyword, id, { relations: ['questions', 'questions.owner'] });
+      const keyword = await manager.findOne(Keyword, id, {
+        relations: ['questions', 'questions.owner'],
+      });
       if (!keyword) {
         throw new NotFoundException(`Keyword '${id}' not found.`);
       }
-      const questions = paginate(keyword.questions, params);
+      let questions = keyword.questions;
+      questions = questions.filter((question) => {
+        return (
+          question.updated_at.getFullYear() === year &&
+          question.updated_at.getMonth() === month
+        );
+      });
+      questions = paginate(questions, params);
       return withCountQuestionsUpvotes(questions, manager);
     });
   }
