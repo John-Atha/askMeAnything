3b61c0834c422006f7eab502b7e2dbe82e2ef2e8
John-Atha <giannisj3@gmail.com>
2021-06-03 18:06:29 +0300

    Add data-layer compatibility to questions resource of analytics comp

--- SOA/analytics/src/question/question.service.ts
+++ SOA/analytics/src/question/question.service.ts
@@ -1,35 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
-import { Question } from './entities/question.entity';
-import { paginate, withCountQuestionsUpvotes } from '../../general-methods/methods';
+import { paginate } from '../../general-methods/methods';
+import { countQuestionsUpvotes, getQuestions } from 'async_calls/async_calls';
 
 
 @Injectable()
 export class QuestionService {
   constructor(@InjectEntityManager() private manager: EntityManager) {}
 
-  async findAll(params, year: number, month: number): Promise<Question[]> {
+  async findAll(params, year: number, month: number): Promise<any> {
     return this.manager.transaction(async (manager) => {
-      let questions = await this.manager.find(Question, { relations: ['owner'] });
+      //let questions = await this.manager.find(Question, { relations: ['owner'] });
+      let questions = await getQuestions({ owner: true });
+      questions = questions.data;
       if (year && month) {
         questions = questions.filter((question) => {
+          const date = new Date(question.created_at);
           return (
-            question.created_at.getFullYear() === year &&
-            question.created_at.getMonth() === month
+            date.getFullYear() === year &&
+            date.getMonth() === month
           );
         });
       }
       else if (year && !month) {
         questions = questions.filter((question) => {
+          const date = new Date(question.created_at);
           return(
-            question.created_at.getFullYear() === year
+            date.getFullYear() === year
           );
         });
       }
       questions.sort((a, b) => (a.created_at > b.created_at) ? -1 : 1 );
       questions = paginate(questions, params);
-      return withCountQuestionsUpvotes(questions, manager);  
+      //return withCountQuestionsUpvotes(questions, manager);
+      questions = await countQuestionsUpvotes(questions);
+      return questions.data;
     })
   }
 
