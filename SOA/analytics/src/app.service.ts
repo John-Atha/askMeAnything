d344d7304000c22ba367859356048a0016189ea6
John-Atha <giannisj3@gmail.com>
2021-06-08 16:18:52 +0300

    Add anals description endpoint

--- SOA/analytics/src/app.service.ts
+++ SOA/analytics/src/app.service.ts
@@ -1,8 +1,119 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  description(reqParams: any): any {
+    if (!reqParams.url || !reqParams.method) {
+      throw new BadRequestException('Url / method not given');
+    }
+    const url = reqParams.url;
+    const method = reqParams.method;
+    console.log(reqParams.url);
+    console.log(reqParams.method);
+    let res = {
+      exists: false,
+      needsAuth: false,
+    }
+    const urls = [
+      {
+        url: new RegExp('questions/monthly/[0-9]+/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('questions/yearly/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('questions'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/questions/monthly/[0-9]+/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/questions/yearly/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/questions'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('answers/monthly/[0-9]+/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('answers/yearly/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('answers'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/answers/monthly/[0-9]+/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/answers/yearly/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/answers'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/answered/monthly/[0-9]+/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/answered/yearly/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('users/[0-9]+/answered'),
+        method: 'get',
+        needsAuth: false,
+      },      
+      {
+        url: new RegExp('keywords/[0-9]+/questions/monthly/[0-9]+/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('keywords/[0-9]+/questions/yearly/[0-9]+'),
+        method: 'get',
+        needsAuth: false,
+      },
+      {
+        url: new RegExp('keywords/[0-9]+/questions'),
+        method: 'get',
+        needsAuth: false,
+      },
+    ]
+    for (let i=0; i<urls.length; i++) {
+      if (urls[i].url.test(url) && urls[i].method===method) {
+        console.log('found at');
+        console.log(urls[i]);
+        res.exists = true;
+        res.needsAuth = urls[i].needsAuth;
+      }
+    }
+    return res;
   }
 }
