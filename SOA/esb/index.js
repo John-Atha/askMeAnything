3b7152a2747a0033eff14e0dfce2e2dd64bacefc
John-Atha <giannisj3@gmail.com>
2021-06-09 14:31:53 +0300

    Add service detection and message forwarding to esb

--- SOA/esb/index.js
+++ SOA/esb/index.js
@@ -31,45 +31,125 @@ const pool = require('redis-connection-pool')('myRedisPool', {
 
 console.log('connected to redis');
 
-async function redirect(req, res){
-  //console.log(req);
-  const url = req.query.url;
-  console.log(url);
+/* function to detect the target service from the request URL query param */
+const findService = (url) => {
+  let dst = null;
   if (url.startsWith(urls.questManUrl)) {
-    //const result = await axios.get(questManUrl, { url, method: 'get' }); 
-    const params = { url, method: 'get' }
-    axios.get(urls.questManUrl, { params })
+    dst = urls.questManUrl;
+  }
+  else if (url.startsWith(urls.questRunUrl)) {
+    dst = urls.questRunUrl;
+  }
+  else if (url.startsWith(urls.analsUrl)) {
+    dst = urls.analsUrl;
+  }
+  else if (url.startsWith(urls.statsUrl)) {
+    dst = urls.statsUrl;
+  }
+  else if (url.startsWith(urls.authUrl)) {
+    dst = urls.authUrl;
+  }
+  return dst;
+}
+
+/* function to forward the requests from the esb to the services */
+async function send_without_auth(method, url, req, res) {
+  if (method === 'get') {
+    axios.get(url, { params: req.query })
+    .then(response => {
+      console.log('Sent response.');
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('Sent error.');
+      res.send(err.response.data);
+    })
+  }
+  else if (method === 'post' || method === 'patch') {
+    const body = req.body;
+    const func = method==='post' ? axios.post : axios.patch;
+    func(url, body)
+    .then(response => {
+      console.log('Sent response.');
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('Sent error.');
+      res.send(err.response.data);
+    })
+  }
+  else /*if (method === 'delete') */ {
+    axios.delete(url)
+    .then(response => {
+      console.log('Sent response.');
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log('Sent error.');
+      res.send(err.response.data);
+    })
+  }}
+
+/* main function to receive the requests  */
+async function req_send(req, method, res) {
+  
+  /* detect target service */
+  const url = req.query.url;
+  console.log('-------------');
+  console.log(`asked url: ${url}`);
+  const dstService = findService(url);
+  console.log(`Dst service: ${dstService}`);
+  if (!dstService) return res.status(404).send('Service not found.');
+  
+  /* validate endpoint from service description */
+  const explore_params = { url, method };
+  if (dstService!==urls.authUrl) {
+    axios.get(dstService, { params: explore_params })
     .then(response => {
       const { exists, needsAuth } = response.data;
       console.log(response.data);
       if (!exists) res.status(404).send('Service not found.');
+      /* if endpoint exists  */
       if (!needsAuth) {
-        axios.get(url, { params: req.query })
-        .then(response => {
-          console.log(response.data);
-          res.send(response.data);
-        })
-        .catch(err => {
-          //console.log(err);
-          res.status(400).send('Could not get data from service');
-        })
+        /* if no auth is needed, forward the request */
+        console.log('No need for auth.');
+        send_without_auth(method, url, req, res);
       }
       else {
+        /* if auth is needed, forward the request to the auth component to retrieve request_user_id */
+
+        /* unauthorized -> return error  */
+
+        /* authorized -> add request_user_id to request header and forward it to the target service  */
+
         return res.status(401).send('Needs authorization.');
       }  
     })
     .catch(err => {
-      console.log(err);
+      console.log(err.response);
       return res.status(400).send('Service unavailable');
     })
   }
   else {
-    return res.status(400).send('Bad request');
+    console.log('No need for auth.');
+    send_without_auth(method, url, req, res);
   }
 }
 
 app.get('/', (req, res) => {
-  return redirect(req, res);
+  return req_send(req, 'get', res);
+});
+
+app.post('/', (req, res) => {
+  return req_send(req, 'post', res);
+});
+
+app.delete('/', (req, res) => {
+  return req_send(req, 'delete', res);
+});
+
+app.patch('/', (req, res) => {
+  return req_send(req, 'patch', res);
 });
 
 app.listen(port, () => {
