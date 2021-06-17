29a774c89bda432b12eb04e57db0e2e181cbfdbb
John-Atha <giannisj3@gmail.com>
2021-06-17 17:37:18 +0300

    Add one-question's answers endpoint

--- microservices/answers/src/question/question.controller.ts
+++ microservices/answers/src/question/question.controller.ts
@@ -1,22 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  ParseIntPipe
+} from '@nestjs/common';
 import { QuestionService } from './question.service';
 
-@Controller('question')
+@Controller('questions')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
-  @Get()
-  findAll() {
-    return this.questionService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.questionService.findOne(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.questionService.remove(+id);
+  @Get(':id/answers')
+  Answers(@Query() reqParams, @Param('id', ParseIntPipe) id: number) {
+    return this.questionService.findQuestionsAnswers(id, reqParams);
   }
 }
--- microservices/answers/src/question/question.service.ts
+++ microservices/answers/src/question/question.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { addNestedOwnerToObj, paginate } from 'general_methods/methods';
+import { EntityManager } from 'typeorm';
+import { Question } from './entities/question.entity';
 @Injectable()
 export class QuestionService {
+  constructor(@InjectEntityManager() private manager: EntityManager) {}
 
-  findAll() {
-    return `This action returns all question`;
+  async findQuestionsAnswers(id: number, paramsInit): Promise<any> {
+    const question = await this.manager.findOne(Question, id, { relations: ['answers', 'answers.owner'] });
+    if (!question) {
+      throw new NotFoundException(`Question ${id} not found.`);
+    }
+    console.log(question.answers);
+    let answers = await this.withCountAnswersUpvotes(question.answers);
+    answers = await this.addNestedOwners(answers);
+    answers = answers.sort((a, b) =>
+      a['upvotesCount'] < b['upvotesCount'] ? 1 : -1,
+    );
+    return paginate(answers, paramsInit);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} question`;
+  async withCountAnswersUpvotes(answers): Promise<any> {
+    for (let i = 0; i < answers.length; i++) {
+      const count = await this.manager.query(
+        `SELECT COUNT(*) FROM public."answer_upvote" WHERE public."answer_upvote"."answerId"=${answers[i].id}`,
+      );
+      console.log('count:');
+      console.log(count);
+      answers[i]['upvotesCount'] = await parseInt(count[0]['count']);
+    };
+    return answers;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} question`;
+  async addNestedOwners(answers: any): Promise<any> {
+    for (let i=0; i<answers.length; i++) {
+      answers[i] = await addNestedOwnerToObj(answers[i], answers[i].owner.id);
+    }
+    return answers;
   }
 }
