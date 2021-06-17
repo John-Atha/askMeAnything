b56a44c1ae57274d8fad97ee90b799849c1d7e15
John-Atha <giannisj3@gmail.com>
2021-06-17 19:08:01 +0300

    Add analytics microservice skeleton

---
+++ microservices/analytics/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { QuestionModule } from './question/question.module';
+import { AnswerModule } from './answer/answer.module';
+import { KeywordModule } from './keyword/keyword.module';
+
+@Module({
+  imports: [UserModule, QuestionModule, AnswerModule, KeywordModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
