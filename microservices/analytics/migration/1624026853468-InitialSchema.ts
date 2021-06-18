562889239a3043aa50c480ecffc01a8ef08ee132
John-Atha <giannisj3@gmail.com>
2021-06-18 19:23:15 +0300

    Add choreographer sync endpoint to analytics micro

--- microservices/analytics/migration/1624026853468-InitialSchema.ts
+++ microservices/analytics/migration/1624026853468-InitialSchema.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class InitialSchema1623945713537 implements MigrationInterface {
-    name = 'InitialSchema1623945713537'
+export class InitialSchema1624026853468 implements MigrationInterface {
+    name = 'InitialSchema1624026853468'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "keyword" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_c37917986ab4672d78f5e037ec3" UNIQUE ("name"), CONSTRAINT "PK_affdb8c8fa5b442900cb3aa21dc" PRIMARY KEY ("id"))`);
