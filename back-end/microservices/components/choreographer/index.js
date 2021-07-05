c4143f1e825c07e27a5de43243a8dc6d31ed2eb5
John-Atha <giannisj3@gmail.com>
2021-07-05 21:39:31 +0300

    Sync choreographer code with production

--- back-end/microservices/components/choreographer/index.js
+++ back-end/microservices/components/choreographer/index.js
@@ -4,33 +4,42 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const port = 3013;
+
+// development
+//const port = 3013;
+// production
+const port = process.env.PORT || 8080;
+
 const corsOptions = {
     origin: [
-        'http://localhost:3008',
-        'http://localhost:3009',
-        'http://localhost:3010',
-        'http://localhost:3011',
-        'http://localhost:3012'
+        'https://askmeanything-micro-auth.herokuapp.com',
+        'https://askmeanything-micro-questions.herokuapp.com',
+        'https://askmeanything-micro-answers.herokuapp.com',
+        'https://askmeanything-micro-statistics.herokuapp.com',
+        'https://askmeanything-micro-analytics.herokuapp.com',
     ],
 };
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const REDIS_PORT = 6379;
+
+// ------ development -------
+/*const REDIS_PORT = 6379;
 const REDIS_HOST = 'localhost';
 const TotalConnections = 50;
-
-const urls = {
-    authMicro: 'http://localhost:3008',
-    questionsMicro: 'http://localhost:3009',
-    answersMicro: 'http://localhost:3010',
-    statsMicro: 'http://localhost:3011',
-    analsMicro: 'http://localhost:3012',
-};
-
 const pool = require('redis-connection-pool')('myRedisPool', {
     host: REDIS_HOST,
     port: REDIS_PORT,
+    max_clients: TotalConnections,
+    perform_checks: false,
+    database: 0,
+});
+console.log('connected to redis');*/
+
+// ------ production -------
+const TotalConnections = 50;
+const pool = require('redis-connection-pool')('myRedisPool', {
+    url: process.env.REDIS_URL,
     max_clients: TotalConnections,
     perform_checks: false,
     database: 0,
@@ -96,5 +105,5 @@ app.post('/choreo', cors(corsOptions), (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Choreographer listening at https://askmeanything-micro-choreo.herokuapp.com:${port}`)
 });
