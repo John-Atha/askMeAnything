52ed4a3da3abefadb6a2e18cd36a3686bfba6c42
John-Atha <giannisj3@gmail.com>
2021-07-05 23:09:00 +0300

    Update micro-urls

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://askmeanything52.herokuapp.com",
     /* SOA back-end */
     esbUrl: "https://askmeanything-soa-esb.herokuapp.com",
     questManUrl: "https://askmeanything-soa-quest-man.herokuapp.com",
@@ -7,9 +7,9 @@ module.exports = {
     questRunUrl: "https://askmeanything-soa-quest-run.herokuapp.com",
     statsAnalsUrl: "https://askmeanything-soa-anals-stats.herokuapp.com",
     /* Microservices back-end*/
-    microAuthUrl: "http://localhost:3008",
-    microQuestUrl: "http://localhost:3009",
-    microAnswersUrl: "http://localhost:3010",
-    microStatsUrl: "http://localhost:3011",
-    microAnalsUrl: "http://localhost:3012",
+    microAuthUrl: "https://askmeanything-micro-auth.herokuapp.com",
+    microQuestUrl: "https://askmeanything-micro-questions.herokuapp.com",
+    microAnswersUrl: "https://askmeanything-micro-answers.herokuapp.com",
+    microStatsUrl: "https://askmeanything-micro-statistics.herokuapp.com",
+    microAnalsUrl: "https://askmeanything-micro-analytics.herokuapp.com",
 }
