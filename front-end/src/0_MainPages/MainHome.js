887fbf8c18984c0aa0634d8c657c0521e1a99d84
John-Atha <giannisj3@gmail.com>
2021-07-06 12:01:06 +0300

    Add carousel responsiveness to home page

--- front-end/src/0_MainPages/MainHome.js
+++ front-end/src/0_MainPages/MainHome.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AnswerQuestion from './AnswerQuestion';
 import AskQuestion from './AskQuestion';
 import PeriodQuestions from './PeriodQuestions';
 import KeywordQuestions from './KeywordQuestions';
 import StatsCarousel from '../4_Profile/StatsCarousel';
+import Carousel from 'react-bootstrap/Carousel';
+import './styles.css';
 
 function MainHome() {
 
-    return(
-        <div className="main-page center-content">
-            <div className='flex-layout center-content'>
-                <AskQuestion />
-                <AnswerQuestion />
-                <PeriodQuestions />
-                <KeywordQuestions />
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+
+    if (window.innerWidth >= 500) {
+        return(
+            <div className="main-page center-content">
+                <div className='flex-layout center-content'>
+                    <AskQuestion />
+                    <AnswerQuestion />
+                    <PeriodQuestions />
+                    <KeywordQuestions />
+                </div>
+                <div className='margin-top' />
+                <StatsCarousel case='home-page' />
             </div>
-            <div className='margin-top' />
-            <StatsCarousel case='home-page' />
-        </div>
-    )
+        )
+    }
+    else {
+        return(
+            <div className="main-page center-content">
+                <Carousel activeIndex={index} onSelect={handleSelect} className='carousel' id='carousel-responsive'>
+                    <Carousel.Item interval={4000}>
+                        <AskQuestion />
+                    </Carousel.Item>
+                    <Carousel.Item interval={4000}>
+                        <AnswerQuestion />
+                    </Carousel.Item>
+                    <Carousel.Item interval={4000}>
+                        <PeriodQuestions />
+                    </Carousel.Item>
+                    <Carousel.Item interval={4000}>
+                        <KeywordQuestions />
+                    </Carousel.Item>
+                </Carousel>
+                <div className='margin-top' />
+                <StatsCarousel case='home-page' />
+            </div>
+        )
+    }
 }
 
 export default MainHome;
--- front-end/src/0_MainPages/MyHome.js
+++ front-end/src/0_MainPages/MyHome.js
@@ -6,16 +6,17 @@ import AnswerQuestion from './AnswerQuestion';
 import AskQuestion from './AskQuestion';
 import MyQuestions from './MyQuestions';
 import MyContributions from './MyContributions';
-//import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
 import StatsCarousel from '../4_Profile/StatsCarousel';
+import './styles.css';
 
 function MyHome() {
-    //const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
     const [userId, setUserId] = useState(null);
     
-    //const handleSelect = (selectedIndex, e) => {
-    //  setIndex(selectedIndex);
-    //};
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
     
     useEffect(() => {
         isLogged()
@@ -26,36 +27,47 @@ function MyHome() {
             window.location.href='/';
         })
     }, [])
-        /*
-            <Carousel activeIndex={index} onSelect={handleSelect} className='carousel' style={{'height': '400px'}}>
-                <Carousel.Item interval={5000}>
+    
+    if (window.innerWidth>=500) {
+        return(
+            <div className="main-page center-content">
+                <div className='flex-layout center-content'>
                     <AskQuestion />
-                </Carousel.Item>
-                <Carousel.Item interval={5000}>
-                        <AnswerQuestion />
-                </Carousel.Item>
-                <Carousel.Item interval={5000}>
+                    <AnswerQuestion />
                     <MyQuestions id={userId} />
-                </Carousel.Item>
-                <Carousel.Item>
                     <MyContributions id={userId} />
-                </Carousel.Item>
-            </Carousel>
-        */
-    return(
-        <div className="main-page center-content">
-            <div className='flex-layout center-content'>
-                <AskQuestion />
-                <AnswerQuestion />
-                <MyQuestions id={userId} />
-                <MyContributions id={userId} />
+                </div>
+                <div className='margin-top' />
+                { userId && 
+                    <StatsCarousel case='questions' user={ { id: userId } } />            
+                }
             </div>
-            <div className='margin-top' />
-            { userId && 
-                <StatsCarousel case='questions' user={ { id: userId } } />            
-            }
-        </div>
-    )
+        )
+    } 
+    else {
+        return (
+            <div className="main-page center-content">
+                <Carousel activeIndex={index} onSelect={handleSelect} className='carousel' style={{'height': '400px'}}>
+                    <Carousel.Item interval={5000}>
+                        <AskQuestion />
+                    </Carousel.Item>
+                    <Carousel.Item interval={5000}>
+                            <AnswerQuestion />
+                    </Carousel.Item>
+                    <Carousel.Item interval={5000}>
+                        <MyQuestions id={userId} />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <MyContributions id={userId} />
+                    </Carousel.Item>
+                </Carousel>
+                <div className='margin-top' />
+                { userId && 
+                    <StatsCarousel case='questions' user={ { id: userId } } />            
+                }
+            </div>
+        )
+    }  
 }
 
 export default MyHome;
